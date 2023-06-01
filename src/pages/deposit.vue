44f21a7113cf6ddeee0c1f22afcfadf1b1b43b2f
jane99j <jaynerrch@gmail.com>
2023-06-01 14:21:33 +0700

    add deposit

---
+++ src/pages/deposit.vue
@@ -0,0 +1,130 @@
+<template>
+    <section>
+        <div class="mx-auto max-w-screen-2xl px-4 lg:px-12 p-6">
+            <div class="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
+                <!-- Start coding here -->
+                <div class="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+                    <h5 class="text-3xl font-bold text-gray-900 dark:text-white ml-2">
+                        ถอนเงิน
+                    </h5>
+                    <hr class="my-5" />
+                    <div class="flex flex-col items-center p-4 space-y-6 md:flex-row md:space-y-0 md:space-x-4">
+                        <div class="w-full md:w-1/2">
+                            <form class="flex items-center">
+                                <label for="simple-search" class="sr-only">ค้นหา</label>
+                                <div class="relative w-full">
+                                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                            fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <input type="text" id="simple-search"
+                                        class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Search" >
+                                </div>
+                            </form>
+                        </div>
+                        <div
+                            class="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
+                            <button type="button"
+                                class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                                <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <path clip-rule="evenodd" fill-rule="evenodd"
+                                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                                </svg>
+                                ค้นหา
+                            </button>
+                            <div class="flex items-center w-full space-x-3 md:w-auto">
+
+                                <!-- Dropdown menu -->
+                                <div id="filterDropdown"
+                                    class="z-10 hidden w-48 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                                        Category
+                                    </h6>
+                                    <ul class="space-y-2 text-sm" aria-labelledby="dropdownDefault">
+                                        <li class="flex items-center">
+                                            <input id="apple" type="checkbox" value=""
+                                                class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                            <label for="apple"
+                                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                                Apple (56)
+                                            </label>
+                                        </li>
+                                        <li class="flex items-center">
+                                            <input id="fitbit" type="checkbox" value=""
+                                                class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                            <label for="fitbit"
+                                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                                Fitbit (56)
+                                            </label>
+                                        </li>
+                                        <li class="flex items-center">
+                                            <input id="dell" type="checkbox" value=""
+                                                class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                            <label for="dell"
+                                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                                Dell (56)
+                                            </label>
+                                        </li>
+                                        <li class="flex items-center">
+                                            <input id="asus" type="checkbox" value="" checked
+                                                class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                            <label for="asus"
+                                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                                Asus (97)
+                                            </label>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
+                <div
+                    class="max-w-screen-xl px-4 mx-auto lg:px-12 mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div class="flex justify-between">
+                        <div>
+                            <h5 class="mr-10 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                เลขที่บัญชี : 25XXX-XX-XXX
+                            </h5>
+                            <h5 class="mr-10 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                ชื่อ-สกุล : นายกล้วย หอม
+                            </h5>
+                        </div>
+                        <div>
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                ประเภทบัญชี : ออมทรัพย์
+                            </h5>
+
+                            <p class="mb-2 text-2xl font-medium tracking-tight text-red-900 dark:text-white">
+                                *มีการถอนแล้ว</p>
+
+
+                        </div>
+                    </div>
+                    <NuxtLink to='/withdraw'>
+                    <div class="flex justify-end">
+                        <div
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            ถอนเงิน
+                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </div>
+                    </div></NuxtLink>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+  
