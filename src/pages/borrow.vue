0908ba38ffcc6bf47fb59e2b29c2c28e9bd17670
zxc2040 <107449746+zxc2040@users.noreply.github.com>
2023-06-02 17:42:13 +0700

    update borrow

--- src/pages/borrow.vue
+++ src/pages/borrow.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="bg-gray-50 dark:bg-gray-900 h-screen flex items-center">
+    <section class="bg-gray-50 dark:bg-gray-900 h-screen items-center">
         <div class="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
             <!-- Start coding here -->
             <div class="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
@@ -25,21 +25,18 @@
                     <div
                         class="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
 
-
-                        <!-- Modal toggle -->
                         <div class="flex justify-center m-5">
-                            <button id="successButton" data-modal-toggle="successModal"
+                            <a id="successButton" data-modal-toggle="successModal" href="/borrow2"
                                 class="block text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                 type="button">
-                                Show success message
-                            </button>
+                                ค้นหา
+                            </a>
                         </div>
 
-                        <!-- Main modal -->
-                        <div id="successModal" tabindex="-1" aria-hidden="true"
+                        <!-- <div id="successModal" tabindex="-1" aria-hidden="true"
                             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                             <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-                                <!-- Modal content -->
+
                                 <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
                                     <button type="button"
                                         class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -51,28 +48,29 @@
                                                 clip-rule="evenodd"></path>
                                         </svg>
                                         <span class="sr-only">Close modal</span>
-                                </button>
-                                <div
-                                    class="w-12 h-12 rounded-full bg-green-100 dark:bg-green-900 p-2 flex items-center justify-center mx-auto mb-3.5">
-                                    <svg aria-hidden="true" class="w-8 h-8 text-green-500 dark:text-green-400"
-                                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                    <span class="sr-only">Success</span>
+                                    </button>
+                                    <div
+                                        class="w-12 h-12 rounded-full bg-green-100 dark:bg-green-900 p-2 flex items-center justify-center mx-auto mb-3.5">
+                                        <svg aria-hidden="true" class="w-8 h-8 text-green-500 dark:text-green-400"
+                                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                clip-rule="evenodd"></path>
+                                        </svg>
+                                        <span class="sr-only">Success</span>
+                                    </div>
+                                    <p class="mb-4 text-lg font-semibold text-gray-900 dark:text-white">Successfully removed
+                                        product.</p>
+                                    <a data-modal-toggle="successModal" type="button" href="/borrow2"
+                                        class="py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-900">
+                                        Continue
+                                    </a>
                                 </div>
-                                <p class="mb-4 text-lg font-semibold text-gray-900 dark:text-white">Successfully removed
-                                    product.</p>
-                                <a data-modal-toggle="successModal" type="button" href="/borrow2"
-                                    class="py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-900">
-                                    Continue
-                                </a>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section></template>
+    </section>
+</template>
